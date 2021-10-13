4622f1250342614bdd00539747ec6ebcfa2b3056
Hyy2001X <1804430051@qq.com>
2021-10-13 10:33:20 +0800

    luci-app-autoupdate: 优化代码

--- luci-app-autoupdate/luasrc/model/cbi/autoupdate/main.lua
+++ luci-app-autoupdate/luasrc/model/cbi/autoupdate/main.lua
@@ -16,15 +16,13 @@ enable.optional = false
 
 proxy = s:option(Flag, "proxy", translate("Preference Mirror Speedup"),translate("Preference Mirror for speeding up downloads while upgrading (For Mainland)"))
 proxy.default = 1
+proxy:depends("enable", "1")
 proxy.optional = false
 
 forceflash = s:option(Flag, "forceflash", translate("Preference Force Flashing"),translate("Preference Force Flashing while firmware upgrading (DANGEROUS)"))
 forceflash.default = 0
+forceflash:depends("enable", "1")
 forceflash.optional = false
-
-github = s:option(Value,"github",translate("Github Url"),translate("For detecting cloud version and downloading firmware"))
-github.default = github_url
-github.rmempty = false
 
 week = s:option(ListValue,"week",translate("Update Day"),translate("Recommend to set AUTOUPDATE time at an idle time"))
 week:value(7,translate("Everyday"))
@@ -36,20 +34,22 @@ week:value(5,translate("Friday"))
 week:value(6,translate("Saturday"))
 week:value(0,translate("Sunday"))
 week.default = 0
+week:depends("enable", "1")
 
 hour = s:option(Value,"hour",translate("Hour"))
 hour.datatype = "range(0,23)"
-hour.rmempty = false
+hour.rmempty = true
+hour.default = 0
+hour:depends("enable", "1")
 
 minute = s:option(Value,"minute",translate("Minute"))
 minute.datatype = "range(0,59)"
-minute.rmempty = false
+minute.rmempty = true
+minute.default = 30
+minute:depends("enable", "1")
 
-s:option(Flag, "enable_autocheck", translate("Enable AutoCheck Updates"),translate("When enabled, a cyclic task is set separately to Check Updates"))
-
-autocheck = s:option(Value,"autocheck",translate("Check Freq"),translate("Check updates only period periodically in hours"))
-autocheck.datatype = "range(1,23)"
-autocheck.rmempty = true
-autocheck:depends("enable_autocheck", "1")
+github = s:option(Value,"github",translate("Github Url"),translate("For detecting cloud version and downloading firmware"))
+github.default = github_url
+github.rmempty = false
 
 return m
