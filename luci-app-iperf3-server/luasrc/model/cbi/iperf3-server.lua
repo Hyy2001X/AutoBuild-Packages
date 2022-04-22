a0c641fb1c573a87a4e8ca15532fadbec3947798
Hyy2001X <1804430051@qq.com>
2022-04-22 14:11:55 +0800

    luci-app-iperf3-server: 添加多个服务实例支持

--- luci-app-iperf3-server/luasrc/model/cbi/iperf3-server.lua
+++ luci-app-iperf3-server/luasrc/model/cbi/iperf3-server.lua
@@ -1,4 +1,4 @@
-m = Map("iperf3-server", translate("iPerf3 Server"), translate("iPerf - The ultimate speed test tool for TCP, UDP and SCTP"))
+m = Map("iperf3-server", translate("iPerf3 Server"), translate("iPerf3 - The ultimate speed test tool for TCP, UDP and SCTP"))
 
 m:section(SimpleSection).template = "iperf3-server/iperf3-server_status"
 
@@ -6,18 +6,31 @@ s = m:section(TypedSection,"iperf3-server","")
 s.addremove = false
 s.anonymous = true
 
-enable = s:option(Flag, "enable", translate("Enable"))
-enable.default = "0"
-enable.rmempty = false
+main_enable = s:option(Flag, "main_enable", translate("Enable"), translate("Enable iPerf3 Servers"))
+main_enable.default = "0"
+main_enable.rmempty = false
 
-port = s:option(Value, "port", translate("Port"), translate("iPerf3 Server listening port"))
+s = m:section(TypedSection, "servers", translate("Server Settings"), translate("Set up Multi-iPerf3 Servers"))
+s.anonymous = true
+s.addremove = true
+s.template = "cbi/tblsection"
+
+enable_server = s:option(Flag, "enable_server", translate("Enable"))
+enable_server.default = "1"
+enable_server.rmempty = false
+
+port = s:option(Value, "port", translate("Port"))
 port.datatype = "port"
 port.default = "5201"
-port.optional = false
 port.rmempty = false
 
-options = s:option(Value, "options", translate("Extra options"), translate("Incorrect options may cause iPerf3 Server to fail to start"))
-options.optional = true
-options.rmempty = true
+delay = s:option(Value, "delay", translate("Start delay"))
+delay.default = "0"
+delay.datatype = "uinteger"
+delay.rmempty = false
+
+extra_options = s:option(Value, "extra_options", translate("Extra Options"))
+extra_options.rmempty = true
+extra_options.password= false
 
 return m
