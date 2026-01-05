2738e56f27de51f9d2b3e4547dff80abf6011161
Hyy2001X <1804430051@qq.com>
2026-01-05 18:40:48 +0800

    luci-app-shutdown: fix typo

--- luci-app-shutdown/luasrc/model/cbi/shutdown.lua
+++ luci-app-shutdown/luasrc/model/cbi/shutdown.lua
@@ -1,5 +1,5 @@
 m = Map("system", translate("Shutdown / Reboot"),
-    translate("Use the buttons below to reboot or shut down the device. Actions are executed via XHR without reloading the page."))
+	translate("Use the buttons below to reboot or shut down the device."))
 
 s = m:section(SimpleSection)
 s.template = "shutdown/actions"
